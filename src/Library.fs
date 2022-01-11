2436c6d8327d82c5d6961bd0cc9ac6f7fb3c4a01
Kai Ito <kai.ito@gmail.com>
2022-01-11 21:47:21 +0100

    Add utilities fs

--- src/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Fable.Etebase
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/Utilities.fs
@@ -0,0 +1,8 @@
+﻿namespace Fable.Etebase
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+module Utilities =
+    let toBase64 (data: obj) : string = import "toBase64" "etebase"
+    let fromBase64 (encoded: string) : 'a = import "fromBase64" "etebase"
