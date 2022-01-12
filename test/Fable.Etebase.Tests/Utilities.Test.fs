1a8b01de94c83214d8d3dd15502b92d3de7a61e7
Kai Ito <kai.ito@gmail.com>
2022-01-12 21:28:31 +0100

    Add more utilities and test

--- test/Fable.Etebase.Tests/Utilities.Test.fs
+++ test/Fable.Etebase.Tests/Utilities.Test.fs
@@ -6,17 +6,37 @@ open Fable.Etebase
 Jest.describe (
     "Base64 encoding",
     fun () ->
-        Jest.test (
-            "Should encode to base64",
+        Jest.beforeAll (
             async {
                 // Required for weird WASM behaviour
                 do! Async.Sleep 500
-                let email = "test+something@example.com"
-                let encoded = Utilities.toBase64 (email)
+            }
+        )
+
+        Jest.test (
+            "Should encode to base64",
+            fun () ->
+                let toEncode = [| 1uy; 2uy; 3uy |]
+                let encoded = Utilities.toBase64 (toEncode)
+
+                Jest.expect(encoded).toEqual ("AQID")
+        )
+
+        Jest.test (
+            "Should decode from base64",
+            fun () ->
+                let encoded = "AQID"
+                let decoded = Utilities.fromBase64 (encoded)
 
                 Jest
-                    .expect(encoded)
-                    .toEqual ("dGVzdCtzb21ldGhpbmdAZXhhbXBsZS5jb20")
-            }
+                    .expect(decoded)
+                    .toMatchObject ([| 1uy; 2uy; 3uy |])
+        )
+
+        Jest.test (
+            "Should generate randomy bytes",
+            fun () ->
+                let randomBytes = Utilities.randomBytes 32
+                Jest.expect(randomBytes).toHaveLength (32)
         )
 )
