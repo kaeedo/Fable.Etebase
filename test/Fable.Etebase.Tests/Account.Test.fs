38dac31ac2fc033b8d1a133d155595bb8e2a111d
Kai Ito <kai.ito@zuehlke.com>
2022-01-13 12:50:46 +0100

    Add account test file

---
+++ test/Fable.Etebase.Tests/Account.Test.fs
@@ -0,0 +1,22 @@
+module Account.Test
+
+open Fable.Jester
+open Fable.Etebase
+
+Jest.describe (
+    "Account tests",
+    fun () ->
+        Jest.test (
+            "Should encode to base64",
+            async {
+                // Required for weird WASM behaviour
+                do! Async.Sleep 500
+                let email = "test+something@example.com"
+                let encoded = Utilities.toBase64 (email)
+
+                Jest
+                    .expect(encoded)
+                    .toEqual ("dGVzdCtzb21ldGhpbmdAZXhhbXBsZS5jb20")
+            }
+        )
+)
