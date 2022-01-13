d4e7bfd25b710c8ce78250d0eb05e7d7e190fc90
Kai Ito <kai.ito@zuehlke.com>
2022-01-13 16:10:48 +0100

    Expose more account apis

--- src/Account.fs
+++ src/Account.fs
@@ -7,9 +7,35 @@ open Fable.Core.JsInterop
 //https://github.com/fable-compiler/fable3-samples/blob/main/interop/public/MyClass.js
 //https://github.com/fable-compiler/fable3-samples/blob/main/interop/src/App.fs
 
+type User = {| username: string; email: string |}
+
+type LoginResponseUser =
+    {| username: string
+       email: string
+       pubkey: byte array
+       encryptedContent: byte array |}
+
+
 type Account =
+    abstract serverUrl: string with get, set
+    abstract user: LoginResponseUser with get, set
+    abstract authToken: string option with get, set
+
+    //save(encryptionKey_?: Uint8Array): Promise<base64>;
+    //getCollectionManager(): CollectionManager;
+    //getInvitationManager(): CollectionInvitationManager;
+    abstract logout: unit -> Promise<unit>
+    abstract fetchToken: unit -> Promise<unit>
+    abstract changePassword: string -> Promise<unit>
+    abstract getDashboardUrl: unit -> Promise<string>
+
+
+type IAccount =
     abstract login: username: string * password: string * ?serverUrl: string -> Promise<Account>
+    abstract signup: user: User * password: string * ?serverUrl: string -> Promise<Account>
     abstract isEtebaseServer: serverUrl: string -> Promise<bool>
+// static restore(accountDataStored_: base64, encryptionKey_?: Uint8Array): Promise<Account>;
 
 module Account =
-    let account: Account = jsNative
+    [<Import("Account", "Etebase")>]
+    let account: IAccount = jsNative
--- test/Fable.Etebase.Tests/Account.Test.fs
+++ test/Fable.Etebase.Tests/Account.Test.fs
@@ -3,10 +3,16 @@ module Account.Test
 open Fable.Jester
 open Fable.Etebase
 
+let username = ""
+let password = ""
+let email = ""
+
 Jest.describe (
     "Account tests",
     fun () ->
-        Jest.test (
+
+        Jest.test.skip (
+            // TODO: Something something cross origin null
             "Should be valid server",
             (promise {
                 do!
@@ -16,8 +22,100 @@ Jest.describe (
 
                 do!
                     Jest
-                        .expect(Account.account.isEtebaseServer ("https://api.etebase.com/partner/etesync/"))
+                        .expect(Account.account.isEtebaseServer ("https://api.etebase.com"))
                         .resolves.toBeTruthy ()
+            })
+
+        )
+
+        Jest.test (
+            "Should login",
+            (promise {
+                let! loggedIn = Account.account.login (username, password)
+
+                Jest
+                    .expect(loggedIn.serverUrl)
+                    .toEqual ("https://api.etebase.com")
+
+                Jest
+                    .expect(loggedIn.user.pubkey)
+                    .toHaveLength (32)
+
+                Jest
+                    .expect(loggedIn.user.encryptedContent)
+                    .toHaveLength (104)
+
+                Jest
+                    .expect(loggedIn.user.username)
+                    .toEqual (username)
+
+                Jest.expect(loggedIn.user.email).toEqual (email)
+
+                do! loggedIn.logout ()
+            })
+        )
+
+        Jest.test.skip (
+            // TODO: run tests against localhost
+            "Should Signup",
+            (promise {
+                let user: Fable.Etebase.User =
+                    {| email = email
+                       username = Utilities.toBase64 (email) |}
+
+                let! response = Account.account.signup (user, password)
+
+                Fable.Core.JS.console.log (response)
+
+                Jest.expect("loggedIn.user.email").toEqual (email)
+            })
+
+        )
+
+        Jest.test (
+            "Should get dashboard url",
+            (promise {
+                let! loggedIn = Account.account.login (username, password)
+
+                let! url = loggedIn.getDashboardUrl ()
+
+                Jest
+                    .expect(url.Substring(0, 53))
+                    .toEqual ("https://dashboard.etebase.com/user/partner/dashboard/")
+
+                do! loggedIn.logout ()
+            })
+
+        )
+
+        Jest.test (
+            "Should logout",
+            (promise {
+                let! loggedIn = Account.account.login (username, password)
+
+                Jest.expect(loggedIn.authToken).not.toBeNull ()
+
+                do! loggedIn.logout ()
+
+                Jest.expect(loggedIn.authToken).toBeNull ()
+            })
+
+        )
+
+        Jest.test (
+            "Should fetch token",
+            (promise {
+                let! loggedIn = Account.account.login (username, password)
+
+                Jest.expect(loggedIn.authToken).not.toBeNull ()
+                loggedIn.authToken <- None
+                Jest.expect(loggedIn.authToken).toBeUndefined ()
+
+                do! loggedIn.fetchToken ()
+
+                Jest.expect(loggedIn.authToken).not.toBeNull ()
+
+                do! loggedIn.logout ()
             })
 
         )
